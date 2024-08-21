161cab5dafb054ee88160b920e4abfe1f8056a39
Adityakapadne08 <142308808+Adityakapadne08@users.noreply.github.com>
2024-08-21 23:38:59 +0530

    created Database connection.

--- api/index.js
+++ api/index.js
@@ -1,8 +1,16 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
 
-const app= express();
+dotenv.config();
+mongoose.connect(
+ process.env.MONGO
+).then(()=>{console.log('mongodb connected');
 
-app.listen(3000,()=>{
-    console.log('server started!');
-
+}).catch((err)=>{
+    console.log(err);
+})
+const app = express();
+app.listen(3000, () => {
+  console.log("server started!");
 });

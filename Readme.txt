09de46b0071df7068bd626d9d481ee40fb6415d4
Adityakapadne08 <142308808+Adityakapadne08@users.noreply.github.com>
2024-08-22 01:27:50 +0530

    create test api route

--- Readme.txt
+++ Readme.txt
@@ -1,1 +1,7 @@
-create model: kind of rules set that let us set that users cant access some part of database . 
+*create model: kind of rules set that let us set that 
+users cant access some part of database . 
+like if mentioned type to be string requird is true or 
+value have to be unique such kind of info cant be
+ changed further 
+
+ *create test api route -best practise is to create other folder for each section of routes
--- api/index.js
+++ api/index.js
@@ -1,16 +1,23 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
-
+import userRoutes from './routes/user.route.js'
 dotenv.config();
-mongoose.connect(
+mongoose
+.connect(
  process.env.MONGO
-).then(()=>{console.log('mongodb connected');
+).then(()=>{
+    console.log('mongodb connected');
 
-}).catch((err)=>{
+})
+.catch((err)=>{
     console.log(err);
 })
 const app = express();
 app.listen(3000, () => {
   console.log("server started!");
+});
+
+app.use('/api/user',(req,res)=>{
+    res.json({message:'Api is working'});
 });
